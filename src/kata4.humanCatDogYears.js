29d4f45cb51c6c3c56851f34f14a7e8da2cf6fc8
David Ding <dingtechxing16@gmail.com>
2023-03-07 23:34:37 +0000

    Completed humanCatDogYears exercise

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,21 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+    let catAge = 0
+    let dogAge = 0;
+
+ if(number === 1){
+   catAge = 15;
+   dogAge = 15;
+ } else if (number === 2){
+    catAge = 15 + 9;
+    dogAge = 15 + 9;
+ } else {
+    catAge = 15 + 9 + (4 * (number - 2));
+    dogAge = 15 + 9 + (5 * (number - 2));
+ }
+
+ return [number, catAge, dogAge];
+
+};
+
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+ it("returns the age of cat and dog with given formula", () => {
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+        expect(humanCatDogYears(20)).toEqual([20, 96, 114])
+        expect(humanCatDogYears(30)).toEqual([30, 136, 164])
+ })
+});
