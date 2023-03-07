46965f0f5b328a487ebdd5ee3acf102fe2b14d41
David Ding <dingtechxing16@gmail.com>
2023-03-07 21:08:38 +0000

    Completed fizzBuzz and booleanToWord

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,21 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
 
+const divisibleByThree = (number % 3) === 0
+const divisibleByFive = (number % 5) === 0
+
+if (divisibleByThree && divisibleByFive){
+    return 'FizzBuzz';
+}
+
+if (divisibleByFive){
+    return 'Buzz';
+}
+
+if (divisibleByThree){
+    return 'Fizz';
+}
+
+    return number;
+};
+ 
 module.exports = fizzBuzz;
