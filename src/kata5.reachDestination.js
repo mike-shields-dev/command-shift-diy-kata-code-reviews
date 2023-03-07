b246fbadab631f96712dae39b71c4af17e0ee838
David Ding <dingtechxing16@gmail.com>
2023-03-07 23:57:28 +0000

    Used string literals and math.ceil to solve reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const time = distance/speed;
+    const timeToDestination = Math.ceil(time * 2) / 2;
+    return `I should be there in ${timeToDestination} hours.`
+};
 
 module.exports = reachDestination;
