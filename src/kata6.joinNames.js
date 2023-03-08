366dacc68bb9c1b6b4d8c6215974ee3149b979c7
David Ding <dingtechxing16@gmail.com>
2023-03-08 12:45:03 +0000

    Completed joinNames exercise

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,20 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+//if the array is empty, returns an empty string
+    if (namesObj.length === 0){
+        return ''
+    }
+
+//if the array has 1 name, returns that name only
+    if (namesObj.length === 1){
+        return namesObj[0].name;
+    }
+
+
+//if the array has more than 1 name, need to apply given rules
+    const lastNameInArray = namesObj.pop().name;
+    const stringOfNames = namesObj.map(({name}) => name).join(', ');
+
+    return `${stringOfNames} & ${lastNameInArray}`;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,25 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    const names1 = [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}];
+    const result1 = 'Bart, Lisa & Maggie';
+    expect(joinNames(names1)).toEqual(result1);
+
+    const names2 = [{name: 'Jules'}, {name: 'Desmond'}, {name: 'Jess'}, {name: 'Julian'}];
+    const result2 = 'Jules, Desmond, Jess & Julian';
+    expect(joinNames(names2)).toEqual(result2);
+
+    const names3 = [{name: 'David'}, {name: 'Warren'}, {name: 'Jeff'}, {name: 'Bill'}, {name: 'Developer'}];
+    const result3 = 'David, Warren, Jeff, Bill & Developer';
+    expect(joinNames(names3)).toEqual(result3);
+
+    const names4 = [];
+    const result4 = '';
+    expect(joinNames(names4)).toEqual(result4);
+
+    const names5 = [{name: 'Tech'}];
+    const result5 = 'Tech';
+    expect(joinNames(names5)).toEqual(result5);
+  });
 });
