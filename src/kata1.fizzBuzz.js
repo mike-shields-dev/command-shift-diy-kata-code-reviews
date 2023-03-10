cabd9e13c5acc8c437368111c42e8489188598ff
Max Tauroza <maxtauroza@gmail.com>
2023-03-10 10:46:24 +0000

    refactored for all tests for negative integers and positive integers

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -2,7 +2,7 @@ const fizzBuzz = (number) => {
   if (number % 3 === 0 && number % 5 !== 0) {
     return "Fizz";
   }
-  if (number % 5 === 0 && number % 3 > 0) {
+  if (number % 5 === 0 && number % 3 !== 0) {
     return "Buzz";
   }
   if (number % 5 === 0 && number % 3 === 0) {
--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -3,14 +3,17 @@ const { fizzBuzz } = require("../src");
 describe("fizzBuzz", () => {
   it("returns Fizz when passed a multiple of 3", () => {
     expect(fizzBuzz(-9)).toBe("Fizz");
+    expect(fizzBuzz(9)).toBe("Fizz")
   });
 
   it("returns Buzz when passed a multiple of 5", () => {
     expect(fizzBuzz(25)).toBe("Buzz");
+    expect(fizzBuzz(-25)).toBe("Buzz")
   });
 
   it("returns FizzBuzz when passed a multiple 3 and 5", () => {
     expect(fizzBuzz(30)).toBe("FizzBuzz");
+    expect(fizzBuzz(-30)).toBe("FizzBuzz")
   });
 
   it("returns the number when it isn't a multiple of 3 or 5", () => {
