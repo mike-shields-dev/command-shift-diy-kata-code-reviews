6175c912e2f9aa892ef13420a48825b7fc374010
Max Tauroza <maxtauroza@gmail.com>
2023-03-10 13:44:24 +0000

    added extra test for HumanCatDogYears to ensure functionality, test passed

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -3,5 +3,6 @@ const { humanCatDogYears } = require("../src");
 describe("humanCatDogYears", () => {
   it("returns array of human, cat and dog years when passed human years", () => {
     expect(humanCatDogYears(12)).toEqual([12, 64, 74]);
+    expect(humanCatDogYears(17)).toEqual([17, 84, 99]);
   });
 });
