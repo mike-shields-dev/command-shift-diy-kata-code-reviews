32940ebaf5496816f30b5b4754efedbcb893f058
Max Tauroza <maxtauroza@gmail.com>
2023-03-10 14:03:32 +0000

    added functionality to reachDestination, passed test

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const timeToDestination = distance / speed;
+  const roundedTime = Math.round(timeToDestination * 2) / 2;
+  return `I should be there in ${roundedTime} hours`;
+};
 
 module.exports = reachDestination;
