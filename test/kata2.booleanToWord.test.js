e4b305630fc1da08573485131c6487ef696c13c7
Max Tauroza <maxtauroza@gmail.com>
2023-03-10 10:56:34 +0000

    initial commit with tests written

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,7 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns Yes when passed true and returns No when passed false");
+  expect(booleanToWord(true)).toBe("Yes");
+  expect(booleanToWord(false)).toBe("No");
 });
