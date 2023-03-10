bd05603a18fdfb5ef53e88ff88d92083a1cdf29b
Max Tauroza <maxtauroza@gmail.com>
2023-03-10 13:41:42 +0000

    added functionality, passed test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,11 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  const firstTwoYears = 15 + 9;
+  const animalYears = function(catOrDog) {
+    return catOrDog * (number - 2) + firstTwoYears;
+  }
+  const catYears = animalYears(4);
+  const dogYears = animalYears(5);
+  return Array.of(number, catYears, dogYears);
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -2,6 +2,6 @@ const { humanCatDogYears } = require("../src");
 
 describe("humanCatDogYears", () => {
   it("returns array of human, cat and dog years when passed human years", () => {
-    expect(humanCatDogYears(12).toEqual([12, 64, 74]));
+    expect(humanCatDogYears(12)).toEqual([12, 64, 74]);
   });
 });
