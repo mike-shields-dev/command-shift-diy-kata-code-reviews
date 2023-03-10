828d82d57cfcdbef0022c22030da3c88278fd53f
Max Tauroza <maxtauroza@gmail.com>
2023-03-10 11:03:40 +0000

    corrected test syntax by putting expect in a callback function

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,7 +1,8 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  it("returns Yes when passed true and returns No when passed false");
-  expect(booleanToWord(true)).toBe("Yes");
-  expect(booleanToWord(false)).toBe("No");
+  it("returns Yes when passed true and returns No when passed false", () => {
+    expect(booleanToWord(true)).toBe("Yes");
+    expect(booleanToWord(false)).toBe("No");
+  });
 });
