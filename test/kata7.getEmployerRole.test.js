bb9c0b3a796b3ef2460f871c618bed5626f08383
Max Tauroza <maxtauroza@gmail.com>
2023-03-13 11:12:04 +0000

    Refactored test to make more concise and more readable

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,14 +1,13 @@
 const { getEmployerRole } = require("../src");
+const employees = [
+  { name: "Sally", role: "Duty Manager" },
+  { name: "Orson", role: "Bartender" },
+  { name: "Sandor", role: "Kitchen Manager" },
+  { name: "Paulina", role: "Door Supervisor" },
+];
 
 describe("getEmployerRole", () => {
   test("returns the employee's role in the company", () => {
-    expect(
-      getEmployerRole("Orson", [
-        { name: "Sally", role: "Duty Manager" },
-        { name: "Orson", role: "Bartender"},
-        { name: "Sandor", role: "Kitchen Manager"},
-        { name: "Paulina", role: "Door Supervisor" },
-      ])
-    ).toEqual("Bartender");
+    expect(getEmployerRole("Orson", employees)).toEqual("Bartender");
   });
 });
