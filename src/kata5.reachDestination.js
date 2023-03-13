298f021406a47e11017637817ffd778bf178c21b
Max Tauroza <maxtauroza@gmail.com>
2023-03-13 09:40:59 +0000

    changed functionality for reachDestination, now passing test

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,7 +1,6 @@
 const reachDestination = (distance, speed) => {
   const timeToDestination = distance / speed;
-  const roundedTime = Math.round(timeToDestination * 2) / 2;
+  const roundedTime = Math.ceil(timeToDestination * 2) / 2;
   return `I should be there in ${roundedTime} hours`;
 };
-
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -2,6 +2,7 @@ const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
-    expect(reachDestination(57, 18)).toBe("I should be there in 3 hours");
+    expect(reachDestination(57, 18)).toBe("I should be there in 3.5 hours");
+    expect(reachDestination(99, 7)).toBe("I should be there in 14.5 hours");
   });
 });
