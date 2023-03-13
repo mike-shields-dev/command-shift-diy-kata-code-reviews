76063eb071c729edb9bbe8c935d0845a8669d228
Max Tauroza <maxtauroza@gmail.com>
2023-03-13 10:57:34 +0000

    added functionality for joinNames, passed test

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,6 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  const nameString = namesObj.map((object) => String(object.name));
+  return `${nameString.slice(0, -1).join(", ")} & ${nameString.slice(-1)}`;
+};
 
 module.exports = joinNames;
