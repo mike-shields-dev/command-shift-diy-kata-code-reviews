04920553837184bbc5a6f10c82e40d9d4c052863
Max Tauroza <maxtauroza@gmail.com>
2023-03-13 10:19:06 +0000

    Wrote test for kata6.joinNames

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,7 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{name: 'John'}, {name: 'Judith'}, {name: 'Antoinette'}]).toEqual("John, Judith /& Antoinette");
+  });
 });
